24c08c465cb6886eda9ac2e6374b597463f60f40
Devin Moore <devinmoore@google.com>
2021-11-08 17:52:47 +0000

    daisy: Switch to new AIDL IR service
    Test: atest VtsHalIrTargetTest hal_implementation_test
    Bug: 205000342
    Change-Id: I70c54aa8e8962939cc8f4b42fbbe828e9dad7f0d

--- device.mk
+++ device.mk
@@ -69,8 +69,7 @@ PRODUCT_PACKAGES += \
 
 # ConsumerIr
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Properties
 -include device/xiaomi/tissot/prop.mk
